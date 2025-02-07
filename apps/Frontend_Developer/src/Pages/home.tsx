85cd55902ab8256d67b7fc86e0b020f9dcfdd2cf
saugat-baidwar100 <saugatbaidwar97.skillprompt@gmail.com>
2025-02-07 16:02:43 +0545

    Now complete setup of frontend folder structure with testing

--- apps/Frontend_Developer/src/Pages/home.tsx
+++ apps/Frontend_Developer/src/Pages/home.tsx
@@ -1,3 +1,10 @@
+import { NavBar } from '../Components/navbar';
+
 export function Home() {
-  return <h1>hello I am home page</h1>;
+  return (
+    <>
+      <h1>hello I am home page</h1>
+      <NavBar />
+    </>
+  );
 }
